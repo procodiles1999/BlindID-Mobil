85c4ce0e37e00f6ef8d36d8e13db7396a919a2bf
procodiles1999 <procodiles3@gmail.com>
2026-01-13 03:03:03 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -5,53 +5,41 @@ import 'package:permission_handler/permission_handler.dart';
 void main() {
   runApp(MaterialApp(
     debugShowCheckedModeBanner: false,
-    home: DebugScreen(),
+    home: BlindWebScreen(),
     theme: ThemeData.dark(),
   ));
 }
 
-class DebugScreen extends StatefulWidget {
+class BlindWebScreen extends StatefulWidget {
   @override
-  _DebugScreenState createState() => _DebugScreenState();
+  _BlindWebScreenState createState() => _BlindWebScreenState();
 }
 
-class _DebugScreenState extends State<DebugScreen> {
+class _BlindWebScreenState extends State<BlindWebScreen> {
   late final WebViewController controller;
-  String durumMesaji = "Başlatılıyor...";
-  bool hataVarMi = false;
 
   @override
   void initState() {
     super.initState();
     izinIste();
-
-    // TEST İÇİN GOOGLE'I KULLANIYORUZ
-    String testLink = 'https://www.google.com';
+    
+    // LINKIN HAZIR, BURAYA DOKUNMA
+    String siteLink = 'https://blind-id-server--procodiles3.replit.app'; 
 
     controller = WebViewController()
       ..setJavaScriptMode(JavaScriptMode.unrestricted)
-      ..setBackgroundColor(const Color(0xFFFF0000)) // Arka plan KIRMIZI (Test için)
+      ..setBackgroundColor(const Color(0x00000000))
       ..setNavigationDelegate(
         NavigationDelegate(
-          onPageStarted: (String url) {
-            setState(() {
-              durumMesaji = "Yükleniyor: $url";
-            });
-          },
           onPageFinished: (String url) {
-            setState(() {
-              durumMesaji = ""; // Yüklendi, yazıyı kaldır
-            });
+            print('Sayfa Yüklendi: $url');
           },
           onWebResourceError: (WebResourceError error) {
-            setState(() {
-              hataVarMi = true;
-              durumMesaji = "HATA: ${error.description}";
-            });
+            print('Hata oluştu: ${error.description}');
           },
         ),
       )
-      ..loadRequest(Uri.parse(testLink));
+      ..loadRequest(Uri.parse(siteLink));
   }
 
   Future<void> izinIste() async {
@@ -61,28 +49,9 @@ class _DebugScreenState extends State<DebugScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.black, // En arka plan siyah
+      backgroundColor: Colors.black,
       body: SafeArea(
-        child: Stack(
-          children: [
-            // 1. KATMAN: WEB SİTESİ
-            WebViewWidget(controller: controller),
-            
-            // 2. KATMAN: BİLGİ EKRANI (Sadece yüklenirken veya hata varsa görünür)
-            if (durumMesaji.isNotEmpty)
-              Center(
-                child: Container(
-                  padding: EdgeInsets.all(20),
-                  color: hataVarMi ? Colors.red : Colors.black54,
-                  child: Text(
-                    durumMesaji,
-                    style: TextStyle(color: Colors.white, fontSize: 16),
-                    textAlign: TextAlign.center,
-                  ),
-                ),
-              ),
-          ],
-        ),
+        child: WebViewWidget(controller: controller),
       ),
     );
   }
